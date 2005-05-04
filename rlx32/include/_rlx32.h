540c510faf11fb248c067e96e828bc37ff25ef37
maffy <maffy>
2005-05-04 00:03:55 +0000

    Fix Linux build.

--- rlx32/include/_rlx32.h
+++ rlx32/include/_rlx32.h
@@ -94,7 +94,7 @@ Prepared for public release: 02/24/2004 - Stephane Denis, realtech VR
 
 /* Data Types */
 #if !defined __STDC_VERSION__ || __STDC_VERSION__ < 199901L
-    #if defined __LCC__ || defined LINUX || (defined __BEOS__ && B_BEOS_VERSION >=0x0520)
+    #if defined __LCC__ || defined __LINUX__ || (defined __BEOS__ && B_BEOS_VERSION >=0x0520)
        #include <stdint.h>
        #include <stddef.h>       
     #elif defined __APPLE__ && defined __MACH__
@@ -121,7 +121,7 @@ Prepared for public release: 02/24/2004 - Stephane Denis, realtech VR
        /* unsigned 64 bit type. */
        typedef unsigned __int64		u_int64_t;
     #endif // __GNUC__
-   #endif // LINUX
+   #endif // __LINUX__
 #endif // STDC_VERSION
 
 
